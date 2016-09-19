e321a514cf35a90e90d2f5ea1fb756334c129883
Andrew Diaz <ZaidWerd@Andrews-MBP.home>
2016-09-19 06:51:59 -0400

    created game logic

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -2,3 +2,26 @@ var cardOne = 'queen';
 var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
+
+if (cardOne === cardTwo) {
+	alert('You found a match!');	
+} else {
+  alert('Sorry, try again.');
+}
+
+if (cardThree === cardFour) {
+	alert('You found a match!');	
+} else {
+  alert('Sorry, try again.');
+}
+
+var board = document.getElementById('game-board');
+
+function createBoard() {
+	for (var i=0; i<cards.length; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		board.appendChild(cardElement);
+  }
+
+}
